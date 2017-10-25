bc171e784a2d013c3f58a7742a46c0bbdeb3cc5d
Biggs <Zachary.Biggs@stlcop.local>
2017-10-24 23:03:28 -0500

    Added in the max height portion, still was unable to match the online output of the mean heights however

--- number3.R
+++ number3.R
@@ -2,3 +2,7 @@ read.csv("shrub-volume-experiment.csv")
 by_site <- group_by(shrub, site)
 avg_height <- summarize(by_site, avg_height = mean(height))
 print(avg_height)
+
+max_height <- summarize(by_site, max_height = max(height))
+print(max_height)
+
